8e76fccde58dfe347e2a79f057873f8bd1e00714
Muthu Kumar <mkumar.gt@zoho.com>
2019-01-05 01:12:37 +0530

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -6,11 +6,11 @@ import os
 name=sys.argv[1]
 name=os.path.basename(name)
 name=name.replace(" ","%20")
-url="http://www.omdbapi.com/?t="+name
+url="http://www.omdbapi.com/?apikey=[YOUR-API-KEY]&t="+name
 r=urllib.urlopen(url)
 web=r.read()
 web=web.decode('UTF-8')
 
-m=re.findall(r'tt[0-9]+',web)
+m=re.findall(r'tt[0-9]+',web) #regex to find the IMDB ID
 url='http://www.imdb.com/title/'+m[0]+'/'
 w.open_new_tab(url)
